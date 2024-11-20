7214ba91b8971f4a33e7afe01e0588351b888b1c
Wendey Ricardo <wendeyricardo@gmail.com>
2024-11-20 00:26:53 -0300

    add commons project and finishing consumer and producer service

--- consumer-service/src/main/java/com/wendey/consumer_service/rabbitmq/consumers/ProductConsumer.java
+++ consumer-service/src/main/java/com/wendey/consumer_service/rabbitmq/consumers/ProductConsumer.java
@@ -1,5 +1,6 @@
 package com.wendey.consumer_service.rabbitmq.consumers;
 
+import dtos.ProductDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,11 @@ import static constants.RabbitMQConstants.RK_PRODUCT_LOG;
 
 @Log4j2
 @Component
-public class StringConsumer {
+public class ProductConsumer {
 
     @RabbitListener(queues = {RK_PRODUCT_LOG})
-    public void consumerLog(String message) {
-        log.info("Consumer received a message " + message);
+    public void consumerLog(ProductDTO message) {
+        log.info("Received message " + message.toString());
     }
 
 }
